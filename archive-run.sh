d489eb623afa0663a2eaeeac7e6340bdbdba22c9
Stuart Grieve <s.grieve@qmul.ac.uk>
2020-07-27 15:56:50 +0100

    adding bash controllers for the python code

---
+++ archive-run.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+python rainfall-archive.py 2852TH-level-stage-i-15_min-mASD /Users/stuart/ea_api/data 2020 1 1
+python rainfall-archive.py 2859TH-level-downstage-i-15_min-mASD /Users/stuart/ea_api/data 2020 1 1
+python rainfall-archive.py 2859TH-level-stage-i-15_min-mASD /Users/stuart/ea_api/data 2020 1 1
+python rainfall-archive.py 278744TP-rainfall-tipping_bucket_raingauge-t-15_min-mm /Users/stuart/ea_api/data 2020 1 1
---
+++ borehole-getter.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+python rainfall-archive.py SP90_64-level-groundwater-i-1_h-mAOD /Users/stuart/ea_api/data 2019 7 28
+python rainfall-archive.py SP90_56-level-groundwater-i-1_h-mAOD /Users/stuart/ea_api/data 2019 7 28
