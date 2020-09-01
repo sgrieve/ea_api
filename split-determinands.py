160221bd32c5818830ad31e28a9a85ff78684074
Stuart Grieve <s.grieve@qmul.ac.uk>
2020-09-01 20:53:56 +0100

    fixing hardcoded filename in split determinand code and adding some better error trapping

--- split-determinands.py
+++ split-determinands.py
@@ -9,7 +9,7 @@ for filename in filenames:
 
     determinand_ids = ['0076', '0077', '0085', '0117', '9901', '9924']
 
-    df = pd.read_csv('/Users/stuart/ea_api/data/TH-PCNR0012.csv', dtype={'determinand_id': str})
+    df = pd.read_csv(filename, dtype={'determinand_id': str})
 
     for d_id in determinand_ids:
 
@@ -17,6 +17,7 @@ for filename in filenames:
 
         out_name = '{}_{}.csv'.format(os.path.splitext(filename)[0], d_id)
 
-        print(out_name, min(tmp_df['sample_date_time'].values), max(tmp_df['sample_date_time'].values), tmp_df['determinand_name'].values[0])
-
-        tmp_df.to_csv(out_name, index=False)
+        if len(tmp_df['sample_date_time']) > 0:
+            tmp_df.to_csv(out_name, index=False)
+        else:
+            print('No data in', out_name, 'for determinand_id', d_id)
